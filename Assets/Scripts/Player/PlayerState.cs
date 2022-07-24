2f233922ea13df532f53874cb7d77f6532699bc9
FamiDev <daniel.windhede@gmail.com>
2022-07-25 01:12:23 +0200

    split classes to separate files

---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,5 @@
+﻿public class PlayerState {
+    public virtual void OnEnter(PlayerController controller) { }
+    public virtual void OnUpdate(PlayerController controller, PlayerData data) { }
+    public virtual void OnExit(PlayerController controller) { }
+}
